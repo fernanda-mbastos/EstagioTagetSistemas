2cdf55b733e0a65515dd59223a19de6c7792c152
fernanda-mbastos <nanda.541989@gmail.com>
2024-03-13 13:00:09 -0300

    ATUALIZACOES

--- 03_QUESTAO/scripts.js
+++ 03_QUESTAO/scripts.js
@@ -1,19 +1,13 @@
-
-
-
-
 /*3) Descubra a lógica e complete o próximo elemento:
 
+a) 1, 3, 5, 7, 9 // numeros primos
 
+b) 2, 4, 8, 16, 32, 64, 128 // potencias de 2: 2^1, 2^2, 2^3, 2^4, ....
 
-a) 1, 3, 5, 7, ___
+c) 0, 1, 4, 9, 16, 25, 36, 49 // soma o anterior ao proximo numero impar 1=0+1; 4=1+3; 9=4+5;16=9+7...
 
-b) 2, 4, 8, 16, 32, 64, ____
+d) 4, 16, 36, 64, 100 // 2^2, 4^2, 6^2, 8^2, proximo 10^2
 
-c) 0, 1, 4, 9, 16, 25, 36, ____
+e) 1, 1, 2, 3, 5, 8, 13 // sequencia de Fibonacci
 
-d) 4, 16, 36, 64, ____
-
-e) 1, 1, 2, 3, 5, 8, ____
-
-f) 2, 10, 12, 16, 17, 18, 19, ____*/
+f) 2, 10, 12, 16, 17, 18, 19, 200 // proximo numero que comeca com a letra D */
